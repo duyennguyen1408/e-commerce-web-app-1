fad3e479219eb03453c749c4dee7d1f31a434346
teq-duyennguyen <duyennguyen@teqnological.asia>
2023-10-23 22:14:10 +0700

    Add project code

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import axios from "axios"
+import Product from "@/components/Product";
+
+export default async function Home() {
+  const response = await axios.get("https://fakestoreapi.com/products");
+  const products: productsDataType[] = await response.data;
+
+  return (
+    <main className="min-h-screen max-w-7xl mx-auto px-8 xl:px-0 mt-48">
+      <section className="flex flex-col space-y-12 pb-44">
+        <h1 className="text-5xl font-bold text-center">DEALS OF THE DAY</h1>
+        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+}
